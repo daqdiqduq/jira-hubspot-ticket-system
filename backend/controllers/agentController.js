dda0593e4c9b9d6fa895086b4482071c3098765f
Shaddiq <30701070+daqdiqduq@users.noreply.github.com>
2025-08-28 22:26:45 +0700

    Delete backend directory

--- backend/controllers/agentController.js
+++
@@ -1,9 +0,0 @@
-function setupShift(req, res) {
-  res.send('Shift setup');
-}
-
-function getShifts(req, res) {
-  res.send(['Shift1', 'Shift2']);
-}
-
-module.exports = { setupShift, getShifts };
--- backend/routes/agents.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { setupShift, getShifts } = require('../controllers/agentController');
-
-router.post('/shift', setupShift);
-router.get('/shift', getShifts);
-
-module.exports = router;
